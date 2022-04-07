aa37d329c2d2848b50eb3a436497c51aa722b7f5
youssef hassan <youssefhassan369@yahoo.com>
2022-04-07 20:00:09 +0200

    heuristic file added

---
+++ Heuristic.py
@@ -0,0 +1,12 @@
+
+
+
+def Heuristic(node):
+    return FindMatchingRow(node)+FindMatchingCol(node)+FindMatchingDiag(node)   #weights will be added
+def FindMatchingRow(node):
+    #do work
+def FindMatchingCol(node):
+    # do work
+def FindMatchingDiag(node):
+    # do work
+
--- Node.py
+++ Node.py
@@ -1,14 +1,3 @@
-# class State:
-#     def __init__(self, col1, col2, col3, col4, col5, col6, col7):
-#         self.col1 = col1
-#         self.col2 = col2
-#         self.col3 = col3
-#         self.col4 = col4
-#         self.col5 = col5
-#         self.col6 = col6
-#         self.col7 = col7
-
-
 class Node:
     def __init__(self, value, alpha, beta, col1, col2, col3, col4, col5, col6, col7):
         self.value = value
