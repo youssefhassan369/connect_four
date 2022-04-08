5cb5d9640f91e83dcd45855c6a3028984653ad9a
youssef hassan <youssefhassan369@yahoo.com>
2022-04-08 05:02:54 +0200

    minmax without alpha&beta done

--- Node.py
+++ Node.py
@@ -1,4 +1,5 @@
 import copy
+from Node import *
 
 
 def changeCol(col):
@@ -10,39 +11,50 @@ def changeCol(col):
     return "".join(temp)
 
 
+class Child:
+    def __init__(self,col0, col1, col2, col3, col4, col5, col6):
+        self.col0 = col0
+        self.col1 = col1
+        self.col2 = col2
+        self.col3 = col3
+        self.col4 = col4
+        self.col5 = col5
+        self.col6 = col6
+
+
 def GenerateChildren(state):
     list1 = []
     if changeCol(state.col0) != -1:
-        child0 = copy.copy(state)
+        child0 = Child(state.col0, state.col1, state.col2, state.col3, state.col4, state.col5, state.col6)
         child0.col0 = changeCol(state.col0)
         list1.append(child0)
 
     if changeCol(state.col1) != -1:
-        child1 = copy.copy(state)
+        child1 = Child(state.col0, state.col1, state.col2, state.col3, state.col4, state.col5, state.col6)
         child1.col1 = changeCol(state.col1)
         list1.append(child1)
 
     if changeCol(state.col2) != -1:
-        child2 = copy.copy(state)
+        child2 = Child(state.col0, state.col1, state.col2, state.col3, state.col4, state.col5, state.col6)
         child2.col2 = changeCol(state.col2)
         list1.append(child2)
 
     if changeCol(state.col3) != -1:
-        child3 = copy.copy(state)
+        child3 = Child(state.col0, state.col1, state.col2, state.col3, state.col4, state.col5, state.col6)
         child3.col3 = changeCol(state.col3)
         list1.append(child3)
 
     if changeCol(state.col4) != -1:
-        child4 = copy.copy(state)
+        child4 = Child(state.col0, state.col1, state.col2, state.col3, state.col4, state.col5, state.col6)
         child4.col4 = changeCol(state.col4)
         list1.append(child4)
     if changeCol(state.col5) != -1:
-        child5 = copy.copy(state)
+        child5 = Child(state.col0, state.col1, state.col2, state.col3, state.col4, state.col5, state.col6)
         child5.col5 = changeCol(state.col5)
         list1.append(child5)
 
     if changeCol(state.col6) != -1:
-        child6 = copy.copy(state)
+        child6 = Child(state.col0, state.col1, state.col2, state.col3, state.col4, state.col5, state.col6)
         child6.col6 = changeCol(state.col6)
         list1.append(child6)
 
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from functions import *
 from Heuristic import *
+from Node import *
+from MinMax import *
 
 if __name__ == '__main__':
-    board=generateInitialState()
-    print(Heuristic(board))
+    board = generateInitialState()
+    # print(Heuristic(board))
+    MinMax(board, 5, 5)
