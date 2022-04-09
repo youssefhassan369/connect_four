5e2c37fbc43537e4c41ba4f4c113989d971fd4de
youssef hassan <youssefhassan369@yahoo.com>
2022-04-10 01:41:35 +0200

    final commit

--- MinMax.py
+++ MinMax.py
@@ -4,71 +4,78 @@ import numpy as np
 from treelib import Node as N, Tree
 
 
-def Max(node, k, f, tree, count):
-    count += 1
+
+
+def Max(node, k, f,tree,count):
+
+    count+=1
 
     if k == 0:
         computer = Heuristic(node, 'r', 'y')
         user = Heuristic(node, 'y', 'r')
         node.value = computer - 2 * user
 
-        return node.value, count
+        return node.value,count
     list1 = []
     for i in range(len(node.children)):
         temp = Node(0, -99, 99, node.children[i].col0, node.children[i].col1, node.children[i].col2,
                     node.children[i].col3, node.children[i].col4, node.children[i].col5, node.children[i].col6)
         n = tree.create_node("0", temp.id, parent=node.id)
-        value, count = Min(temp, k - 1, f, tree, count)
-        n.tag = value
+        value,count = Min(temp, k - 1, f,tree,count)
+        n.tag=value
         list1.append(value)
-    if len(list1) == 0:
+    if len(list1)==0:
         computer = Heuristic(node, 'r', 'y')
         user = Heuristic(node, 'y', 'r')
         node.value = computer - 2 * user
 
-        return node.value, count
+        return node.value,count
     node.value = max(list1)
     if k == f:
-        return node.children[np.argmax(list1)], node.value, count
-    return node.value, count
+
+        return node.children[np.argmax(list1)], node.value,count
+    return node.value,count
 
 
-def Min(node, k, f, tree, count):
+def Min(node, k, f,tree,count):
+
     count += 1
     if k == 0:
         computer = Heuristic(node, 'r', 'y')
         user = Heuristic(node, 'y', 'r')
         node.value = computer - 2 * user
 
-        return node.value
+        return node.value,count
     list1 = []
     for i in range(len(node.children)):
         temp = Node(0, -99, 99, node.children[i].col0, node.children[i].col1, node.children[i].col2,
                     node.children[i].col3, node.children[i].col4, node.children[i].col5, node.children[i].col6)
-        n = tree.create_node("0", temp.id, parent=node.id)
-        value, count = Max(temp, k - 1, f, tree, count)
-        n.tag = value
+        n=tree.create_node("0", temp.id, parent=node.id)
+        value,count = Max(temp, k - 1, f,tree,count)
+        n.tag=value
         list1.append(value)
-    if len(list1) == 0:
+    if len(list1)==0:
         computer = Heuristic(node, 'r', 'y')
         user = Heuristic(node, 'y', 'r')
         node.value = computer - 2 * user
 
-        return node.value, count
+        return node.value,count
     node.value = min(list1)
     if k == f:
-        return node.children[np.argmin(list1)], node.value, count
-    return node.value, count
+        return node.children[np.argmin(list1)], node.value,count
+    return node.value,count
 
 
 def MinMax(state, k, f):
-    tree = Tree()
-    count = 0
-    n = tree.create_node("0", state.id)
-    state1, value, count = Max(state, k, f, tree, count)
-    state2 = Node(0, 0, 0, state1.col0, state1.col1, state1.col2, state1.col3, state1.col4, state1.col5, state1.col6)
-    n.tag = value
-    tree.show()
-    tree = Tree()
+    tree=Tree()
 
-    return state2, count
+    count=0
+
+    n=tree.create_node("0",state.id)
+    state1, value,count = Max(state, k, f,tree,count)
+    state2 = Node(0, 0, 0, state1.col0, state1.col1, state1.col2, state1.col3, state1.col4, state1.col5, state1.col6)
+    n.tag=value
+    tree.show()
+
+
+    return state2,count
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from Game import *
 from gui import *
 
-
 if __name__ == '__main__':
-    print('Select the mode you want to play')
-    print('1) Minimax without alpha-beta pruning')
-    print('2) Minimax with alpha-beta pruning')
-    mode = input('mode: ')
-    print('Enter Number of levels (k):')
-    levels = input('levels: ')
+    # print('Select the mode you want to play')
+    # print('1) Minimax without alpha-beta pruning')
+    # print('2) Minimax without alpha-beta pruning')
+    # mode = input('mode: ')
+    # print('Enter Number of levels (k):')
+    # levels = input('levels: ')
 
     board = generateInitialState()
 
-    game(board,1,4)
+    game(board,1,3)
+
